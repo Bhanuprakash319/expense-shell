7b74f53d29da855ce6c8d9215c11c9069437ca10
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-27 21:17:08 +0530

    backend

---
+++ backend.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+filename=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$filename-$timestamp.log
+
+r="\e[31m"
+g="\e[32m"
+n="\e[0m"
+
+if [ $userid -eq 0 ]
+then
+    echo -e "$g You are super user $n"
+else
+    echo -e "$r You are not super user$n"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+         echo -e "$2 is $g successfull$n"
+    else
+         echo -e "$2 is $r failure$n"
+         exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>> $logfile
+validate $? "disabling nodejs"
